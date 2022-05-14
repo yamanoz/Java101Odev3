efb8690de619a87362798eaab657ccc05237cc6f
Özgür Yaman <yamanozgur6333@gmail.com>
2022-05-14 17:48:08 +0300

    Kombinasyon hesaplama programı

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,51 @@ public class Main {
 
     public static void main(String[] args){
 
+        int n,r,ntotal=1,rtotal=1,nrtotal=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("n sayisina giriniz :");
+        n=sc.nextInt();
+        System.out.print("r sayisina giriniz :");
+        r=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            ntotal=ntotal*i;
+        }
+        for(int i=1;i<=r;i++){
+            rtotal=rtotal*i;
+        }
+        for(int i=1;i<=(n-r);i++)
+        {
+            nrtotal=nrtotal*i;
+        }
+        double kombinasyon=ntotal/(rtotal*nrtotal);
+        System.out.print("Kombinasyon  :"+kombinasyon);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //usAlma();
+
+
+        //ciftToplam();
+
+
+        //ucdortModalma();
+
+    }
+
+    private static void usAlma() {
         int i;
         Scanner sc =new Scanner(System.in);
         System.out.print("Sinir Giriniz :");
@@ -18,14 +63,6 @@ public class Main {
         for(i = 1; i <= sinir; i *= 5) {
             System.out.println(" 5 in kati  :"+i);
         }
-
-
-
-        //ciftToplam();
-
-
-        //ucdortModalma();
-
     }
 
     private static void ciftToplam() {
