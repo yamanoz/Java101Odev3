cfb2d3dd373a781f893b05c9d145bcd5872f725a
Özgür Yaman <yamanozgur6333@gmail.com>
2022-05-15 14:49:40 +0300

    Basamak değerlerini toplayan program

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,24 @@ public class Main {
 
     public static void main(String[] args){
 
-
-        int total=1,sayi,ussu;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Sayiya  giriniz :");
-        sayi=sc.nextInt();
-        System.out.print("Kuvvete giriniz :");
-        ussu=sc.nextInt();
-        for(int i=1;i<=ussu;i++){
-            total*=sayi;
+        System.out.print("Sayi Giriniz :");
+        int sayi=sc.nextInt();
+        int toplam=0,deger=0;
+
+        while(sayi!=0){
+            deger=sayi%10;
+            sayi=sayi/10;
+            toplam=toplam+deger;
         }
-        System.out.print("Sayinin Ussu :"+total);
+        System.out.print("Toplam :"+toplam);
 
 
 
 
 
+
+        //amstronSayi();
 
 
         //kombinasyonHesaplam();
@@ -32,6 +34,68 @@ public class Main {
 
         //ucdortModalma();
 
+    }
+
+    private static void amstronSayi() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        int number = input.nextInt();
+        int basNumber = 0;
+        int tempNumber = number;
+        int basValue;
+        int result = 0;
+        int basPow;
+
+        while (tempNumber != 0) {
+            tempNumber /= 10;
+            basNumber++;
+        }
+
+        tempNumber = number;
+        while (tempNumber != 0) {
+            basValue = tempNumber % 10;
+            // 1*1*1*1 = 1^4
+            basPow = 1;
+            for (int i = 1; i <= basNumber; i++) {
+                basPow *= basValue;
+            }
+            result += basPow;
+            tempNumber /= 10;
+        }
+
+        if (result == number) {
+            System.out.println(number + " sayısı bir Armstrong sayıdır.");
+        } else {
+            System.out.println(number + " sayısı bir Armstrong sayısı değildir.");
+        }
+ /*
+        int a = 2451, basamakSayisi = 0, numberCounter = 0;
+
+        // Basamak Sayısı Bulma İşlemi
+        // 2451 / 10 = 245
+        // 245 / 10 = 24
+        // 24 / 10 = 2
+        // 2 / 10 = 0
+        // 0 / 10 = 0
+
+        while (a != 0) {
+            a /= 10;
+            numberCounter++;
+        }
+
+        // Bir sayının son basamağını bulma
+        // 2451 % 10 = 1
+        int b = 2451;
+        int c = b % 10;
+
+        int sub = 2, sup = 3;
+        int result = 1;
+        for (int i = 1; i <= sup; i++ ){
+            result *= sub;
+        }
+        System.out.println(result);
+
+         */
     }
 
     private static void kombinasyonHesaplam() {
